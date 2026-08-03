f6c3ef5f7f3c40257fb9aad598a089b669a3e1b2
Adwaidh MT <adwaidhmt8@gmail.com>
2026-08-03 10:45:48 +0530

    Time: 0 ms (100%), Space: 55.2 MB (5.63%) - LeetHub

---
+++ 0709-to-lower-case/0709-to-lower-case.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase()
+
+    
+};
+console.log(toLowerCase("HELLO"));
+console.log(toLowerCase("LOVELY"));
+console.log(toLowerCase("HERE"));
